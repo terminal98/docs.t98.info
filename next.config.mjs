dd552834e2c993628b2ac2ae8c807bf0da4dfcfd
terminal98 <6383265+terminal98@users.noreply.github.com>
2025-03-09 02:09:56 +0900

    fix next.config.mjx

--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,12 @@ import nextra from 'nextra'
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
+
+const withNextra = nextra({
   output: 'export',
   images: {
     unoptimized: true // mandatory, otherwise won't export
-  },
-  // Optional: Change the output directory `out` -> `dist`
-  //distDir: "docs"
-}
-
-const withNextra = nextra({
-  // contentDirBasePath: '/docs' // Or even nested e.g. `/docs/advanced`
+  }
 })
 
 // You can include other Next.js configuration options here, in addition to Nextra settings:
