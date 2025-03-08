4458ac8d0e899fd9e3a57d71a2be366436ddb83c
terminal98 <6383265+terminal98@users.noreply.github.com>
2025-03-09 02:20:00 +0900

    整形

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,5 @@ const nextConfig = {
   //distDir: "docs"
 }
 
-
-
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra(nextConfig)
